9af2beaa760be8a315f8d1263f62e8f97fde3ea7
felipe barreiros <felipe.php@outlook.com>
2018-05-15 10:01:08 -0300

    inicial

---
+++ application/views/partials/menu.php
@@ -0,0 +1,5 @@
+<div class="btn-group">
+    <a href="/" class="btn btn-dark">Home</a>
+    <a href="/users" class="btn btn-dark">Usuários</a>
+    <a href="/posts" class="btn btn-dark">Posts</a>
+</div>
